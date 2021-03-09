2a325d19bae602edfba058b97a73a1d49bcd4a03
stefanbertici <69802287+stefanbertici@users.noreply.github.com>
2021-03-09 22:26:57 +0200

    spring initializer base project

---
+++ src/test/java/bongo/hr/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bongo.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
