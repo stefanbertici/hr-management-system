db24f93b93ee01ebf7b874885bfa7b997381d4c4
ThaRyo <chrisi170@hotmail.com>
2021-03-17 12:11:28 +0100

    add springfox doc and swagger-ui

--- src/main/java/bongo/Main.java
+++ src/main/java/bongo/Main.java
@@ -1,13 +1,37 @@
 package bongo;
 
+import bongo.employee.EmployeeController;
+import bongo.team.TeamController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+		EmployeeController.class,
+		TeamController.class
+})
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+	public Docket hrApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.directModelSubstitute(LocalDate.class, String.class);
+	}
 }
