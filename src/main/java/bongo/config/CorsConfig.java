867347eb3ef6f17f48b1044b3cd8ca1bb6fdd443
ThaRyo <chrisi170@hotmail.com>
2021-03-25 19:28:43 +0100

    Add cors configuration

---
+++ src/main/java/bongo/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package bongo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(corsOrigins)
+                        .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
